b7362eca6a12ed9b8ab4920600c93957a79bd0cd
zippkidd <thomastylek@gmail.com>
2021-11-26 01:45:06 -0500

    inject html with script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('Interesting!');
+
+const heading = document.createElement('h1');
+heading.textContent = 'Interesting!';
+
+const app = document.querySelector('#root');
+app.append(heading);
