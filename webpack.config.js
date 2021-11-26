af4edd0c29e6621ea2549727970325326b1d08a0
zippkidd <thomastylek@gmail.com>
2021-11-26 00:50:15 -0500

    configure webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -8,4 +9,11 @@ module.exports = {
 		path: path.resolve(__dirname, './dist'),
 		filename: '[name].bundle.js',
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Web Boilerplate',
+			template: path.resolve(__dirname, './src/template.html'),
+			filename: 'index.html',
+		})
+	]
 }
